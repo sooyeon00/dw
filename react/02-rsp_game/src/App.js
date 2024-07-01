5cb188832b4cbb86fe17b2664245cb7b18c9381c
bsybsy01 <bsybsy2109@gmail.com>
2024-07-01 17:35:50 +0900

    .

--- react/02-rsp_game/src/App.js
+++ react/02-rsp_game/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>가위바위보</h2>
+      <div>
+        <Board name="나" color="yellow" gameHistory={gameHistory} />
+        <Board name="나" color="yellow" gameHistory={otherGameHistory} />
+      </div>
     </div>
   );
 }
